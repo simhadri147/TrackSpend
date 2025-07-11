9ffb9960236c9ef6fa262b5773edd324e25e6baa
coderskom <coders.kom@gmail.com>
2025-07-11 23:11:55 +0530

    Switched config to use environment variables

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
-# config.py
+# config.py (this replaces your old version)
+import os
 
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = ''
-MYSQL_DB = 'expense_tracker'
-MYSQL_CURSORCLASS = 'DictCursor'
+MYSQL_HOST = os.environ.get('MYSQL_HOST')
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_DB = os.environ.get('MYSQL_DB')
+MYSQL_CURSORCLASS = os.environ.get('MYSQL_CURSORCLASS', 'DictCursor')
 
-# Flask secret key
-SECRET_KEY = '1234567890'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# Mail config (Use Gmail for now)
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 587
-MAIL_USE_TLS = True
-MAIL_USERNAME = 'abhiram09882@gmail.com'       # ⚠️ Your Gmail
-MAIL_PASSWORD = 'ghimrdolykuvvrkg'          # ⚠️ App Password (not Gmail password)
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
